b5c2aca0e4a06c1358fa7b25e0cf0fcb4d7cc363
Ivan Venkov <ivanvenkov9@gmail.com>
2019-11-15 08:58:06 +0200

    ....

---
+++ CocktailMagician.Tests/CocktailMagician.Domain.Services.Tests/BarServiceTests/GetBar_Should.cs
@@ -0,0 +1,24 @@
+﻿using CocktailMagician.Data;
+using CocktailMagician.Domain.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace CocktailMagician.Tests.CocktailMagician.Domain.Services.Tests.BarServiceTests
+{
+    [TestClass]
+    public class GetBar_Should
+    {
+        [TestMethod]
+        public async Task ThrowArgumentException_WhenBarIsMissing()
+        {
+            var testBarID = 1;
+            var options = TestUtilities.GetOptions(nameof(ThrowArgumentException_WhenBarIsMissing));
+            using (var assertContext = new AppDBContext(options))
+            {
+                var sut = new BarService(assertContext);
+                await Assert.ThrowsExceptionAsync<ArgumentException>(() => sut.GetBar(testBarID));
+            }
+        }
+    }
+}
